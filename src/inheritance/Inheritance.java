14f79e4c3d895eab5fced5dfe716797c9edfb4cb
cbommina <cbommina@CBOMMINA-IN.in.oracle.com>
2018-07-14 21:36:34 +0530

    TestNG Code

--- src/inheritance/Inheritance.java
+++ src/inheritance/Inheritance.java
@@ -6,18 +6,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 
 public class Inheritance {
-
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","D:\\Selenium Workspace\\chromedriver_win32 (1)\\chromedriver.exe" );
-		WebDriver driver=new ChromeDriver();
+public WebDriver driver;
+@BeforeTest
+public void initiatebrowser()
+	{
+	    driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium Workspace\\chromedriver_win32 (1)\\chromedriver.exe");
+		
+		
 		driver.get("https://www.crmpro.com/index.html");
+	}
+		@Test
+		 public void CRM(){
+		
 		driver.manage().window().maximize();
 		driver.findElement(By.name("username")).sendKeys("Chaitanya509");
 		driver.findElement(By.xpath(".//*[@id='loginForm']/div/input[2]")).sendKeys("Chaithu$509");
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.findElement(By.xpath(".//*[@id='loginForm']/div/div/input")).click();
+		}
 	}
-}
